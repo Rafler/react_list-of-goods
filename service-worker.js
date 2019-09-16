359f219a2965fab844d4a462f5b7bd80d7c0a12b
Rafler <amuronyk@gmail.com>
2019-09-16 11:43:27 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.bf7ed51aef74ff209cbc738d400074c6.js"
 );
 
 self.addEventListener('message', (event) => {
